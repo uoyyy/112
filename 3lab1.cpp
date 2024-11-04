dc18ca97178c7268229705ca0af910143959136c
uoyyy <pekarevdima5@gmail.com>
2024-11-04 15:28:18 +0300

    Add files via upload

---
+++ 3lab1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    int x, y;
+    cout<<"x:";
+    cin>>x;
+    cout<<"y:";
+    cin>>y;
+    int summ=x+y;
+    cout<<summ<<endl;
+    summ=x-y;
+    cout<<summ<<endl;
+    summ=x*y;
+    cout<<summ<<endl;
+    return 0;
+}
